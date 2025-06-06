80fd6f985a45cc81493c137bd8cc3542e78d6ea6
NudelMaster <yefimnu@mail.tau.ac.il>
2025-06-06 08:43:01 +0200

    fix

--- run_agent.py
+++ run_agent.py
@@ -3,10 +3,10 @@ from config import MAX_EPISODES, SCORE_TO_SOLVE
 
 if __name__ == "__main__":
     agent = Agent()
-
+    
     # Train from scratch:
     agent.train(max_episodes=MAX_EPISODES, score_to_solve=SCORE_TO_SOLVE, resume=False)
-
+    
     # Evaluate (and save a GIF) afterward:
     total_reward = agent.evaluate(model_path="lunar_lander_actor_critic.pth", greedy=True)
 
