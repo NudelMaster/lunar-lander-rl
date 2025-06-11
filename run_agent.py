62c3d4e85a0b795a0b3a20dd0186088f51385729
NudelMaster <yefimnu@mail.tau.ac.il>
2025-06-11 14:31:51 +0200

    changed main

--- run_agent.py
+++ run_agent.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     agent.train(max_episodes=MAX_EPISODES, score_to_solve=SCORE_TO_SOLVE, resume=False)
     
     # Evaluate (and save a GIF) afterward:
-    total_reward = agent.evaluate(model_path="lunar_lander_actor_critic.pth", greedy=True)
+    total_reward = agent.evaluate(model_path="lunar_lander_actor_critic.pth", greedy=False)
 
     # Optionally, continue training later:
     # agent.train(max_episodes=500, resume=True)
